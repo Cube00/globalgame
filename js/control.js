76902845281f122364d1ad3c118b0bbef30252bc
Your Name <coller.vato@gmail.com>
2021-03-13 17:26:39 +0400

    header

---
+++ js/control.js
@@ -0,0 +1,15 @@
+var previewSlides = new Swiper(".preview-slides", {
+    speed: 400,
+    spaceBetween: 10,
+    initialSlide: 0,
+    autoHeight: false,
+    direction: "horizontal",
+    loop: false,
+    autoplay: 0,
+    effect: "slide",
+    spaceBetween: 0,
+    slidesPerView: 5,
+    centeredSlides: false,
+    slidesOffsetBefore: 0,
+    grabCursor: true,
+});
